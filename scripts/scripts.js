c35c5f28c2ed10b4657c0150f3f0fc2c3b496908
DemiFiend01 <maggie.mr1606@gmail.com>
2026-03-05 20:47:55 +0100

    hope it is finally fixed for mobile, logo may still be skewed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,10 @@
 const mobileMenu = document.querySelector('.siteHeader__hamburger');
 const nav = document.querySelector('.siteHeader nav');
 
-mobileMenu.addEventListener('click', () => {
-    const isOpen = nav.classList.toggle('is-open');
-    mobileMenu.setAttribute('aria-expanded', isOpen); //expanded the navigation bar
-    mobileMenu.textContent = isOpen ? '✕' : '☰';
-});
+if (mobileMenu && nav) {
+    mobileMenu.addEventListener('click', () => {
+        const isOpen = nav.classList.toggle('is-open');
+        mobileMenu.setAttribute('aria-expanded', isOpen);
+        mobileMenu.textContent = isOpen ? '✕' : '☰';
+    });
+}
